e69a52bd60aff5c1ca9a93af3b4c9c719d66c90d
youssef-rabbani-1 <93159700+youssef-rabbani-1@users.noreply.github.com>
2021-11-02 11:08:37 -0700

    config

--- config/database.js
+++ config/database.js
@@ -4,12 +4,17 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'postgres',
+        host: env('DATABASE_HOST', 'strapi-database.cftby7cputh3.us-east-2.rds.amazonaws.com'),
+        port: env.int('DATABASE_PORT', '5432'),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', 'youssef'),
+        password: env('DATABASE_PASSWORD', 'DB#dmin123456'),
       },
       options: {
-        useNullAsDefault: true,
+        ssl: false,
       },
     },
   },
 });
+ 
